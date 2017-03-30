f2a8a2f8e8cb22afca305d1f5d25b62ff7ac5248
Andy HaoQi Li <andy.li@movit-tech.com>
2017-03-30 12:55:45 +0800

    test

--- app/Filters.scala
+++ app/Filters.scala
@@ -6,19 +6,12 @@ import play.filters.csrf.CSRFFilter
 import play.filters.headers.SecurityHeadersFilter
 import play.filters.hosts.AllowedHostsFilter
 
-/**
- * Add the following filters by default to all projects
- * 
- * https://www.playframework.com/documentation/latest/ScalaCsrf 
- * https://www.playframework.com/documentation/latest/AllowedHostsFilter
- * https://www.playframework.com/documentation/latest/SecurityHeaders
- */
-class Filters @Inject() (
-  csrfFilter: CSRFFilter,
-  allowedHostsFilter: AllowedHostsFilter,
-  securityHeadersFilter: SecurityHeadersFilter
-) extends DefaultHttpFilters(
-  csrfFilter, 
-  allowedHostsFilter, 
-  securityHeadersFilter
-)
+//class Filters @Inject() (
+//  csrfFilter: CSRFFilter,
+//  allowedHostsFilter: AllowedHostsFilter,
+//  securityHeadersFilter: SecurityHeadersFilter
+//) extends DefaultHttpFilters(
+//  csrfFilter,
+//  allowedHostsFilter,
+//  securityHeadersFilter
+//)
