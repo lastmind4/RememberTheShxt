5387ab98d5d70ca34d9e060111f5c4f1735e3d9f
Andy HaoQi Li <andy.li@movit-tech.com>
2017-04-01 17:56:11 +0800

    test

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,7 +1,6 @@
-package filters
-
 import javax.inject.Inject
 
+import filters.{LoggingFilter, SessionFilter}
 import play.api.http.DefaultHttpFilters
 
 class Filters @Inject()(loggingFilter: LoggingFilter, sessionFilter: SessionFilter) extends DefaultHttpFilters(loggingFilter, sessionFilter)
