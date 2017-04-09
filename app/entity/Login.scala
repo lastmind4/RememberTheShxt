e53368b787e80edf205663cb4cdc28d7368b38c0
leehoawki@gmail.com <kisshot0612>
2017-04-09 15:01:41 +0800

    init

--- app/entity/Login.scala
+++ app/entity/Login.scala
@@ -1,10 +1,3 @@
 package entity
 
-import play.api.libs.json.Json
-
 case class Login(id: Long, name: String, password: String)
-
-object Login {
-  implicit val loginFormat = Json.format[Login]
-}
-
